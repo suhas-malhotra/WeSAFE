35f536d47d5ffb42add9a0fd7469650a98cd0537
suhas-malhotra <76173756+suhas-malhotra@users.noreply.github.com>
2021-05-12 02:50:10 +0530

    v1

--- routes/warrantys.js
+++ routes/warrantys.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const moment = require('moment');
-const nodemailer = require('nodemailer');
-const cron = require('node-cron');
 const router = express.Router();
 
 const catchAsync = require('../utils/catchAsync');
@@ -21,35 +19,6 @@ router.get(
     const warrantys = await Warranty.find({
       owner: userId,
     });
-
-    // cron.schedule('* * * * * *', () => {
-    //   let d = new Date().toISOString();
-    //   for (var i = 0; i < warrantys.length; i++) {
-    //     if (warrantys[i].ISOMailDate === d) {
-    //       const mailOptions = {
-    //         from: 'wesafe.2021@gmail.com',
-    //         to: `${emailID}`,
-    //         subject: 'Important message from WeSAFE',
-    //         text: `your product ${warrantys[i].product} of the company
-    //         ${warrantys[i].company}is about to expire in 7 days`,
-    //       };
-    //       const transporter = nodemailer.createTransport({
-    //         service: 'gmail',
-    //         auth: {
-    //           user: 'wesafe.2021@gmail.com',
-    //           pass: 'pooja0474@',
-    //         },
-    //       });
-    //       transporter.sendMail(mailOptions, (error, info) => {
-    //         if (error) {
-    //           console.log(error);
-    //         } else {
-    //           console.log('email send');
-    //         }
-    //       });
-    //     }
-    //   }
-    // });
 
     res.render('warrantys/index', {
       warrantys,
@@ -88,35 +57,6 @@ router.post(
     const eDate = date.add(warranty.period, 'month').toDate();
     const expDate = dateToISOLikeButLocal(eDate);
     warranty.expiry = expDate;
-
-    // const mailOptions = {
-    //   from: 'suhasmalhotra@wesafe.space',
-    //   to: `${emailID}`,
-    //   subject: 'Important message from WeSAFE',
-    //   text: `You have just added the product ${warranty.product} of ${warranty.company} to the WeSAFE.com`,
-    // };
-
-    // const transporter = nodemailer.createTransport({
-    //   service: 'Gmail',
-    //   auth: {
-    //     user: 'wesafe.2021@gmail.com',
-    //     pass: 'pooja0474@',
-    //   },
-    // });
-
-    // transporter.sendMail(mailOptions, (error, info) => {
-    //   if (error) {
-    //     console.log(error);
-    //   } else {
-    //     console.log('email send');
-    //   }
-    // });
-
-    // const mDate = moment(warranty.expiry);
-    // const fDate = mDate.subtract(7, 'd').toDate();
-    // warranty.ISOMailDate = fDate;
-    // const finalDate = dateToISOLikeButLocal(fDate);
-    // warranty.mailDate = finalDate;
 
     await warranty.save();
     req.flash('success', 'Successfully maded a warranty card');
@@ -165,8 +105,16 @@ router.put(
   validateWarranty,
   catchAsync(async (req, res) => {
     const { id } = req.params;
+    const war = new Warranty({
+      ...req.body.warranty,
+      owner: req.user._doc._id,
+    });
+    const date = moment(war.purchase);
+    const eDate = date.add(war.period, 'month').toDate();
+    const expDate = dateToISOLikeButLocal(eDate);
     const warranty = await Warranty.findByIdAndUpdate(id, {
       ...req.body.warranty,
+      expiry: expDate,
     });
     if (!warranty.owner.equals(req.user._doc._id)) {
       req.flash('error', 'Cannot find the warranty card');
